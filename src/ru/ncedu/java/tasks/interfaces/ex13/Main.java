974a7443f8223a4b11adf5823945b414434dd8cf
AnnBezv1997 <73289700+AnnBezv1997@users.noreply.github.com>
2021-01-12 00:45:58 +0300

    Update Main.java

--- src/ru/ncedu/java/tasks/interfaces/ex13/Main.java
+++ src/ru/ncedu/java/tasks/interfaces/ex13/Main.java
@@ -4,12 +4,13 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import static ru.ncedu.java.tasks.interfaces.ex13.Exercises13.sortDir;
 
 public class Main {
     public static void main(String[] args) {
         File dir = new File(".");
         File[] lst = dir.listFiles();
-        Exercises13.sortDir(lst);
+        sortDir(lst);
         for (File f : lst) {
             System.out.println(f.getName());
         }
