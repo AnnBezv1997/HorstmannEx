0d31d7fc28ffd917b1dc2e9e1cade1b2c6d1d331
AnnBezv1997 <73289700+AnnBezv1997@users.noreply.github.com>
2021-01-18 23:42:37 +0300

    Update Main.java

--- src/ru/ncedu/java/tasks/interfaces/ex11/Main.java
+++ src/ru/ncedu/java/tasks/interfaces/ex11/Main.java
@@ -1,21 +1,18 @@
 package ru.ncedu.java.tasks.interfaces.ex11;
 
 import java.io.File;
-import java.io.FileFilter;
+import java.util.List;
+import static ru.ncedu.java.tasks.interfaces.ex11.Exercises11.getSubdirectoriesThree;
+import static ru.ncedu.java.tasks.interfaces.ex11.Exercises11.getSubdirectoriesTwo;
+
 
 public class Main {
     public static void main(String[] args) {
-        Exercises11 exception11 = new Exercises11();
-        File file = new File(".");
+        List<File> files = getSubdirectoriesThree(".");
+        System.out.println(files.toString());    //[.\.idea, .\out, .\src]
+        List<File> files1 = getSubdirectoriesTwo(".");
+        System.out.println(files.toString());    //[.\.idea, .\out, .\src]
 
-        exception11.getDirectories(file, (path) -> path.isDirectory());
-        exception11.getDirectories(file, File::isDirectory);
-        exception11.getDirectories(file, new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return pathname.isDirectory();
-            }
-        });
     }
-
 }
+
